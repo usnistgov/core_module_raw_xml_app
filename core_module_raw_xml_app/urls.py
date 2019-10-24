1ad8a796627e46803100d32b3d465602067d3dff
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-24 14:50:34 -0400

    fix: revert django upgrade
    Change-Id: I81579171705c56484aeb2b22092ed374659af34b

--- core_module_raw_xml_app/urls.py
+++ core_module_raw_xml_app/urls.py
@@ -1,10 +1,9 @@
 """ Url router for the raw xml module
 """
-
-from django.urls import re_path
+from django.conf.urls import url
 
 from core_module_raw_xml_app.views.views import RawXmlModule
 
 urlpatterns = [
-    re_path(r'module-raw-xml', RawXmlModule.as_view(), name='core_module_raw_xml_view'),
+    url(r'module-raw-xml', RawXmlModule.as_view(), name='core_module_raw_xml_view'),
 ]
