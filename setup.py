01eb89f428c63191c56cc9002cd328e33b0a4d73
Guillaume Sousa <guillaume.sousa@nist.gov>
2022-07-07 18:19:43 +0000

    chore: upgrade 1.15.0
    Change-Id: I629d4f522b5c236c6ea6e4e03cc83578c75abb8f

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_module_raw_xml_app",
-    version="1.14.0",
+    version="1.15.0",
     description="Raw xml module for the parser core project",
     long_description=long_desc,
     author="NIST IT Lab",
